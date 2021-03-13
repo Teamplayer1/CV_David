b4d583c1ad46bf93a48cdafe428899ae1ca14b28
david_hanc <david.hanc10@gmail.com>
2021-03-13 14:16:17 +0100

    2.0

--- CVposledni/End.xaml.cs
+++ CVposledni/End.xaml.cs
@@ -19,22 +19,32 @@ namespace CVposledni
             
         }
 
-        string lat = "50.07794178082049";
-        string lon = "14.426502340895745";
-        string address = "Ve Smečkách 7, 110 00 Nové Město Praha";
+         double latSme = 50.07794178082049;
+         double lonSme = 14.426502340895745;
+         string address = "Ve Smečkách 7, 110 00 Nové Město Praha";
+
+         double latMem = 50.071021063561105;
+         double lonMem = 14.448941280438818;
+         string addressM = "Memos";
 
         private async void Cesta_Clicked(object sender, EventArgs e)
 
         {
+
+            double Distancetowork = Location.CalculateDistance(latSme, lonSme, latMem, lonMem, DistanceUnits.Kilometers);
+
+            var location = new Location(50.071021063561105, 14.448941280438818); //Memos
             
 
-            var location = new Location(50.071021063561105, 14.448941280438818);
-            var options = new MapLaunchOptions { NavigationMode = NavigationMode.None, 
-                                                  Name ="Budoucí práce"};
+            var options = new MapLaunchOptions { Name = "MEMOS s.r.o.", NavigationMode = NavigationMode.None };
+
+            
+            distance.Text = "Vzdálenost od mého místa bydliště do MEMOSU =" + Distancetowork.ToString();
 
             try
             {
                 await Map.OpenAsync(location, options);
+                
             }
             catch (Exception ex)
             {
@@ -47,7 +57,8 @@ namespace CVposledni
 
         private async void Button_Clicked(object sender, EventArgs e)
         {
-            var message = new EmailMessage(Subject.Text, "Dobrý den, tímto emailem Vás chceme informovat, že jste byl přijat. &#10; S pozdravem... ", Email.Text);
+            var message = new EmailMessage(Subject.Text, "Dobrý den, tímto emailem Vás chceme informovat, že jste byl přijat. " +
+                " S pozdravem... ", Email.Text);
             
             await Xamarin.Essentials.Email.ComposeAsync(message);
         }
@@ -63,6 +74,11 @@ namespace CVposledni
                 throw;
             }
             //{Body=Sms.Text, Recipients = new List<string> { Phonenumber.Text } }
+        }
+
+        private async void github_Clicked(object sender, EventArgs e)
+        {
+           await Browser.OpenAsync("https://github.com/Teamplayer1/CV_David", BrowserLaunchMode.SystemPreferred);
         }
 
         //  protected async override void OnAppearing()   pokud chci něco udělat při načtení stránky
